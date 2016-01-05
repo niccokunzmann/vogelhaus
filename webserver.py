aa33b73420cfc63dec243d45c4b64ff7700e87de
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-05 19:27:37 +0000

    holding the image in the RAM

--- webserver.py
+++ webserver.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import time
 import threading
+import subprocess
 
-from bottle import run, request, static_file, route, redirect
+from bottle import run, request, static_file, route, redirect, parse_date, HTTPResponse
 
 location = os.path.dirname(__file__)
 static_files = os.path.join(location, 'static')
 
-refresh_after_seconds = 1
-last_captured = time.time() - refresh_after_seconds
+class Picture:
+    refresh_after_seconds = 5
+    
+    def __init__(self):
+        self.content = self.get_content()
+        self.time = time.time()
+        self.next_refresh_time = self.time + self.refresh_after_seconds
+        
+    @staticmethod
+    def get_content():
+        return subprocess.check_output(
+            "raspistill -o - -e jpg -ex night -w 640 -h 480 -t 100".split(), 
+            stderr = sys.stderr)
+    
+    def is_outdated(self):
+        return self.next_refresh_time < time.time()
+
 capture_lock = threading.Lock()
+capturing = False
+picture = Picture()
 
 @route('/')
 def home():
@@ -21,17 +40,39 @@ def home():
 def static(filename):
     return static_file(filename, root = static_files)
 
+def renew_picture():
+    global capturing, picture
+    with capture_lock:
+        to_capture = not capturing and picture.is_outdated()
+        if to_capture:
+            capturing = True # critical section
+    if to_capture:
+        picture = Picture()
+        capturing = False
+
 @route('/picture.jpg')
 def serve_picture():
-    global last_captured
-    with capture_lock:
-        to_capture = last_captured + refresh_after_seconds < time.time()
-        if to_capture:
-            last_captured = time.time()
-    if to_capture:
-        # TODO: what if capturing lasts longer that refresh_after_seconds?
-        os.system("raspistill -o picture.jpg -e jpg -ex night -w 640 -h 480 -t 100")
-    return static_file('picture.jpg', root = location)
+    renew_picture()
+    _picture = picture
+    
+    # from bottle.static_file
+    headers = dict()
+    headers['Content-Type'] = 'image/jpeg'
+
+    headers['Content-Length'] = clen = len(_picture.content)
+    lm = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(picture.time))
+    headers['Last-Modified'] = lm
+
+    ims = request.environ.get('HTTP_IF_MODIFIED_SINCE')
+    if ims:
+        ims = parse_date(ims.split(";")[0].strip())
+    if ims is not None and ims >= int(picture.time):
+        headers['Date'] = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+        return HTTPResponse(status=304, **headers)
+
+    body = '' if request.method == 'HEAD' else picture.content
+    return HTTPResponse(body, **headers)
+
 
 if __name__ == "__main__":
     # server option: http://bottlepy.org/docs/dev/deployment.html#server-options
