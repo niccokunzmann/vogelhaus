a9aa24edcb3b41f9d1c5dec3d602d44217f84bca
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-05 22:20:41 +0000

    using xmlhttprequest to get picture

--- static/scripts/renew-image.js
+++ static/scripts/renew-image.js
@@ -1,20 +1,82 @@
+function getImageLocation(image) {
+    var location = image.getAttribute("original-src");
+    if (!location) {
+      location = image.getAttribute("src");
+      image.setAttribute("original-src", location);
+    }
+    return location;
+}
 
-var renewImageIntervalID = null;
-// set random renewal to avoid being able to track the time we watch
-var renewalID = Math.floor(Math.random() * 1000000);
+function sendRequestToSource(image, location, callback) {
+  if (image.getAttribute("loading") == "true") {
+    return;
+  }
+  // http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
+  var headers = {};
+  var lastModified = image.getAttribute("lastModified");
+  if (lastModified) {
+    headers["If-Modified-Since"] = lastModified;
+  }
+  var xhttp = new XMLHttpRequest(headers);
+  xhttp.responseType = "blob";
+
+  xhttp.onreadystatechange = function() {
+    // from http://www.w3schools.com/ajax/tryit.asp?filename=tryajax_callback
+    // from https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    if (xhttp.readyState == 4) {
+      image.setAttribute("loading", "false");
+      if (xhttp.status == 200) {
+        callback(xhttp);
+      }
+    }
+  };
+  xhttp.open("GET", location, true);
+  xhttp.send();
+  image.setAttribute("loading", "true");
+}
+
+function setImage(xhttp, image) {
+  // from http://stackoverflow.com/a/19680728
+  window.URL.revokeObjectURL(image.getAttribute("src"));
+  var blob = xhttp.response;
+  var imgSrc = URL.createObjectURL(blob);
+  image.setAttribute("src", imgSrc);
+}
+
+function imageReceived(xhttp, image) {
+  var modificationDate = xhttp.getResponseHeader("Last-Modified");
+  var lastModified = image.getAttribute("lastModified");
+  if (modificationDate) {
+    if (lastModified) {
+      // if two dates are given, we can compare them
+      // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+      if (Date.parse(lastModified) < Date.parse(modificationDate)) {
+        image.setAttribute("lastModified", modificationDate);
+        setImage(xhttp, image);
+      }
+    } else {
+      // we have no information about when the image was modified
+      image.setAttribute("lastModified", modificationDate);
+      setImage(xhttp, image);
+    }
+  } else {
+    // server sends no modification information 
+    // this results in higher bandwidth usage
+    setImage(xhttp, image);
+  }
+}
+
+function renewImage(image) {
+  var location = getImageLocation(image);
+  sendRequestToSource(image, location, function(xhttp){imageReceived(xhttp, image)})
+}
 
 function renewImages() {
-  renewalID++;
   var images = document.getElementsByClassName("renew-image");
   for (var i = 0; i < images.length; i++) {
     var image = images[i];
     // set the original source
-    var source = image.getAttribute("original-src");
-    if (!source) {
-      source = image.getAttribute("src");
-      image.setAttribute("original-src", source);
-    }
-    image.setAttribute("src", source + "?renewal=" + renewalID);
+    renewImage(image)
   }
 }
 
