131bd3062c053bce756af51370e76fddc246d66a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-07 14:01:55 +0000

    removed bug that terminates renewal

--- static/scripts/open-image.js
+++ static/scripts/open-image.js
@@ -5,10 +5,9 @@ function openImage(imageID) {
   // clone element 
   //   http://stackoverflow.com/questions/921290/is-it-possible-to-clone-html-element-objects-in-javascript-jquery
   var newElement = image.cloneNode(true);
-  image.parentElement.insertBefore(newElement, image);
-  image.classList.remove("renew-image");
-  image.removeAttribute("id");
+  newElement.classList.remove("renew-image");
+  newElement.removeAttribute("id");
   // insert at beginning
   //   http://www.javascriptkit.com/javatutors/dom2.shtml
-  savedImages.insertBefore(image, savedImages.firstChild);
+  savedImages.insertBefore(newElement, savedImages.firstChild);
 }
